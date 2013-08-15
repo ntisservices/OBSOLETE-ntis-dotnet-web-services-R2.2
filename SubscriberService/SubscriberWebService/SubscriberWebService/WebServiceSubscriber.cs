59164ed17f54bcc1415aab38fdadb6756f5e50ec
Gareth Munro <gareth.munro@thalesgroup.com>
2013-08-15 18:31:34 +0100

    reimplemented in WCF.
    known bugs : max message receive size too small.

---
+++ SubscriberService/SubscriberWebService/SubscriberWebService/WebServiceSubscriber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SubscriberWebService
+{
+    public class WebServiceSubscriber : subscriberSoap11
+    {
+        public DeliverAverageSpeedFvdResponse1 DeliverAverageSpeedFvd(DeliverAverageSpeedFvdRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DeliverMIDASTrafficDataResponse1 DeliverMIDASTrafficData(DeliverMIDASTrafficDataRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DeliverANPRTrafficDataResponse1 DeliverANPRTrafficData(DeliverANPRTrafficDataRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DeliverAverageSpeedFusedDataResponse1 DeliverAverageSpeedFusedData(DeliverAverageSpeedFusedDataRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DeliverAverageJourneyTimeResponse1 DeliverAverageJourneyTime(DeliverAverageJourneyTimeRequest1 request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
